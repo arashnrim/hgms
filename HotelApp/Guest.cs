3f53bf9b7656d3406d2e8cbd813868a899ac6ec1
Arash <47273556+arashnrim@users.noreply.github.com>
2023-01-18 21:41:58 +0800

    feat: implement Guest class

---
+++ HotelApp/Guest.cs
@@ -0,0 +1,40 @@
+﻿//==========================================================
+// Student Number   : REDACTED
+// Student Name     : Arash Nur Iman
+//==========================================================
+
+namespace HotelApp
+{
+    internal class Guest
+    {
+        private string name;
+        public string Name { get; set; }
+
+        private string passportNum;
+        public string PassportNum { get; set; }
+
+        private Stay hotelStay;
+        public Stay HotelStay { get; set; }
+
+        private Membership member;
+        public Membership Member { get; set; }
+
+        private bool isCheckedin;
+        public bool IsCheckedin { get; set; }
+
+        public Guest() { }
+        public Guest (string n, string pn, Stay hs, Membership m)
+        {
+            Name = n;
+            PassportNum = pn;
+            HotelStay = hs;
+            Member = m;
+            IsCheckedin = true;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Passport number: {PassportNum}, Stay information: {HotelStay.ToString()}, Membership information: {Member.ToString()} Checked in: {(IsCheckedin ? "Yes" : "No")}";
+        }
+    }
+}
