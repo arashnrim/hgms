363e1120aa4b011f86fd96a1bd19a9a8cebd02ab
Arash <47273556+arashnrim@users.noreply.github.com>
2023-01-18 21:39:54 +0800

    feat: partially implement Stay class

---
+++ HotelApp/Stay.cs
@@ -0,0 +1,39 @@
+﻿//==========================================================
+// Student Number   : REDACTED
+// Student Name     : Arash Nur Iman
+//==========================================================
+
+namespace HotelApp
+{
+    internal class Stay
+    {
+        private DateTime checkinDate;
+        public DateTime CheckinDate { get; set; }
+
+        private DateTime checkoutDate;
+        public DateTime CheckoutDate { get; set; }
+
+        // TODO: Implement roomList
+
+        public Stay() { }
+        public Stay(DateTime cid, DateTime cod)
+        {
+            CheckinDate = cid;
+            checkoutDate = cod;
+        }
+
+        // TODO: Implement AddRoom()
+
+        // TODO: Call CalculateCharges() per room and calculate the total
+        public double CalculateTotal()
+        {
+            throw new NotImplementedException();
+        }
+
+        // TODO: Add "Number of rooms: {RoomList.Count}" to ToString()
+        public override string ToString()
+        {
+            return $"Checkin date: {CheckinDate.ToString("dd/MM/yyyy")}, Checkout date: {CheckoutDate.ToString("dd/mm/yyyy")}";
+        }
+    }
+}
